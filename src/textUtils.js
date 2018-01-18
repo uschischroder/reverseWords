970722e7d89d5b89079788598490e1ec69e91b15
Luiz Eduardo <dudu.christo@gmail.com>
2018-01-18 10:23:38 +0100

    TU03 - Change to module structure

--- src/textUtils.js
+++ src/textUtils.js
@@ -1,40 +1,42 @@
-function reverse(param) {
-    var reversed = [];
+var textUtils = function() {
 
-    if (isValid(param)) return '';
-    /*for (var i=param.length-1;i>=0;i--) {
-        reversed += param[i];
-    };*/
+    var reverse = function(param) {
+        var reversed = [];
 
-    param = param.toString().trim();
+        if (isValid(param)) return '';
 
-    for (var left=0,right=param.length-1;left<=right;left++,right--) {
-        reversed[left] = param[right];
-        reversed[right] = param[left];
+        param = param.toString().trim();
+
+        for (var left=0,right=param.length-1;left<=right;left++,right--) {
+            reversed[left] = param[right];
+            reversed[right] = param[left];
+        }
+
+        return reversed.join('');
     }
 
-    return reversed.join('');
+    var reverseRec = function(param, reversed, index) {
+        if (index === 1)
+            return param[0];
+
+        reversed = param[index-1] + reverseRec(param, reversed, --index);
+
+        return reversed;
+    }
+
+    var isValid = function(param) {
+        var valid = !param  ||
+            (typeof(param) !== "string" && typeof(param) !== "number");
+
+        return valid;
+    }
+
+    return {
+        reverse : reverse,
+        reverseRec : reverseRec
+    }
 }
 
-function reverseRec(param, reversed, index) {
-    if (index === 1)
-        return param[0];
-
-    reversed = param[index-1] + reverseRec(param, reversed, --index);
-    return reversed;
+if( typeof module !== "undefined" && "exports" in module) {
+    module.exports = textUtils;
 }
-
-function isValid(param) {
-    var valid = !param  ||
-        (typeof(param) !== "string" && typeof(param) !== "number");
-
-    return valid;
-}
-
-if (typeof module !== 'undefined' && 'exports' in module) {
-    module.exports = reverse;
-}
-/*
-var result = reverse('coffees');
-var result2 = reverse('full');
-*/
--- test/textUtilsTest.js
+++ test/textUtilsTest.js
@@ -1,67 +1,68 @@
-if (!reverse) {
-    var reverse = require('../src/reverseString');
+if (!textUtils) {
+    var textUtils = require('../src/textUtils');
 }
+var textUtilsInstance = new textUtils();
 
 QUnit.test( 'Test reverse() returns empty for empty param.', function( assert ) {
-    var result = reverse('');
+    var result = textUtilsInstance.reverse('');
     assert.equal(result, '')
 });
 
 QUnit.test( 'Test reverse() returns null for a null param.', function( assert ) {
-    var result = reverse(null);
+    var result = textUtilsInstance.reverse(null);
     assert.equal(result, '')
 });
 
 QUnit.test( 'Test reverse() removes trailing spaces.', function( assert ) {
-    result = reverse('   ');
+    result = textUtilsInstance.reverse('   ');
     assert.equal(result, '');
 
-    result = reverse(' aaa  ');
+    result = textUtilsInstance.reverse(' aaa  ');
     assert.equal(result, 'aaa');
 
-    result = reverse('  the bird is flying.  ');
+    result = textUtilsInstance.reverse('  the bird is flying.  ');
     assert.equal(result, '.gniylf si drib eht');
 });
 
 QUnit.test( 'Test reverse() returns empty for boolean param.', function( assert ) {
-    var result = reverse(false);
+    var result = textUtilsInstance.reverse(false);
     assert.equal(result, '');
 
-    var result = reverse(true);
+    var result = textUtilsInstance.reverse(true);
     assert.equal(result, '')
 });
 
 QUnit.test( 'Test reverse() returns correct reversed values for single words.', function( assert ) {
-    var result = reverse('coffee');
+    var result = textUtilsInstance.reverse('coffee');
     assert.equal(result, 'eeffoc');
 
-    result = reverse('house');
+    result = textUtilsInstance.reverse('house');
     assert.equal(result, 'esuoh');
 
-    result = reverse('bird');
+    result = textUtilsInstance.reverse('bird');
     assert.equal(result, 'drib');
 
-    result = reverse('123');
+    result = textUtilsInstance.reverse('123');
     assert.equal(result, '321');
 });
 
 QUnit.test( 'Test reverse() returns correct for a numeric param.', function( assert ) {
-    var result = reverse(122223);
+    var result = textUtilsInstance.reverse(122223);
     assert.equal(result, '322221');
 });
 
 QUnit.test( 'Test reverse() returns correct inverted phrase.', function( assert ) {
-    var result = reverse('The house is falling down!');
+    var result = textUtilsInstance.reverse('The house is falling down!');
     assert.equal(result, '!nwod gnillaf si esuoh ehT');
 
-    result = reverse('The coffee is hot!!!');
+    result = textUtilsInstance.reverse('The coffee is hot!!!');
     assert.equal(result, '!!!toh si eeffoc ehT');
 });
 
 QUnit.test( 'Test reverse() returns correct palindrome.', function( assert ) {
-    var result = reverse('rotator');
+    var result = textUtilsInstance.reverse('rotator');
     assert.equal(result, 'rotator');
 
-    result = reverse('dewed');
+    result = textUtilsInstance.reverse('dewed');
     assert.equal(result, 'dewed');
 });
